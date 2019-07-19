68e72ca3e28456a685ecc43d9e4f95818111c4ed
kamehamehaWangzi <konamikce@qq.com>
2019-07-19 15:13:00 +0800

    first commit

---
+++ src/main/java/com/wode/k8sjenkins/K8sJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.wode.k8sjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sJenkinsApplication.class, args);
+	}
+
+}
